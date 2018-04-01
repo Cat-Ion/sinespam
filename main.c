9a8bc5b4c8c60f1db5cb1078435453d29c3a7933
Valentin Ochs <a@0au.de>
2018-04-01 22:18:25 +0200

    Some changes

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <sys/select.h>
 #include "synthesis.h"
 
+#define BUFFER_SIZE 128
+
 int listen_fd;
+int max_fd;
 int num_clients;
+fd_set read_fds, write_fds;
+
 struct client {
   int fd;
   size_t in_length, out_length;
   struct timespec last_read, last_write;
-  char in_buffer[80];
-  char out_buffer[80];
+  char in_buffer[BUFFER_SIZE];
+  char out_buffer[BUFFER_SIZE];
 } clients[64];
 
 int main() {
